3114576c0fbaae61deb9fe22820e47dfe287f37a
ccmxy <windrainicefire@gmail.com>
2017-05-25 10:35:20 -0700

    Printing 1a

--- blum.cpp
+++ blum.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
+#include <bitset>
 
-int rng()
-{
-  int m = 11*19;
-  int xi = 9;
-  for(int i=0;  i<12; i++) {
-     xi = (xi * xi) % m;
-     std::cout << "The number is: " << xi << std::endl;
-   }
+// unsigned char *Crypt(unsigned char *data, int dataLength, unsigned int initialValue){
+void Crypt(unsigned char *data, int dataLength, unsigned int initialValue){
+  long m = 3792215717; //Holds the value that will serve as the modulus
+  long x = initialValue; //Holds current "value" that is squared and modulus'd by m
+
+    for(int i = 0;  i < dataLength; i++) {
+      x = (x * x) % m;
+      //XOR the current value with the ascii value of the (i + 1)th letter
+      int xoredValue = (x ^ 0x41);
+      //Grab the lowest byte from the Xored value to serve as the byte key
+      unsigned byteKey =(xoredValue) & 0xFF;
+      std::cout << std::hex << byteKey << std::endl << std::endl;
+
+    }
+  return;
+
 }
 
-
 int main() {
-  rng();
+  unsigned char *data;
+  char const apple = 'a';
+  data[0] = apple;
+
+  int dataLength = 1;
+
+  unsigned int initialValue = 65536;
+  Crypt(data, dataLength, initialValue);
+
   return 0;
 }
