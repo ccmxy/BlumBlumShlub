b85a4bc09d999851a70c751c754aa74ae28206ae
ccmxy <windrainicefire@gmail.com>
2017-05-30 10:20:01 -0700

    Works well

--- blum.cpp
+++ blum.cpp
@@ -1,33 +1,46 @@
 #include <iostream>
-#include <bitset>
 
-// unsigned char *Crypt(unsigned char *data, int dataLength, unsigned int initialValue){
-void Crypt(unsigned char *data, int dataLength, unsigned int initialValue){
-  long m = 3792215717; //Holds the value that will serve as the modulus
+unsigned char *Crypt(unsigned char *data, int dataLength, unsigned int initialValue){
+  long m = 0xE2089EA5; //Holds the value that will serve as the modulus
   long x = initialValue; //Holds current "value" that is squared and modulus'd by m
+  int i; //loop counter
 
-    for(int i = 0;  i < dataLength; i++) {
-      x = (x * x) % m;
-      //XOR the current value with the ascii value of the (i + 1)th letter
-      int xoredValue = (x ^ data[i]);
-      //Grab the lowest byte from the Xored value to serve as the byte key
-      unsigned byteKey =(xoredValue) & 0xFF;
-      std::cout << std::hex << byteKey << std::endl << std::endl;
+  for(i = 0;  i < dataLength; i++) {
+    x = (x * x) % m;
+    //XOR the current value with the ascii value of the (i + 1)th letter
+    int xoredValue = (x ^ data[i]);
+    //Grab the lowest byte from the Xored value to serve as the byte key
+    unsigned byteKey =(xoredValue) & 0xFF;
+    data[i] = byteKey;
+    std::cout << std::hex << byteKey << std::endl << std::endl;
 
-    }
-  return;
+  }
+  return data;
 
 }
 
 int main() {
   unsigned char *data;
-  char const apple = 'A';
-  data[0] = apple;
+  int dataLength = 5;
+  int i; //loop counter
 
-  int dataLength = 1;
+  data[0] = 'a';
+  data[1] = 'p';
+  data[2] = 'p';
+  data[3] = 'l';
+  data[4] = 'e';
+
+  // data[0] = 0x3a;
+  // data[1] = 0xcb;
+  // data[2] = 0x68;
+  // data[3] = 0x10;
+  // data[4] = 0x84;
+
 
   unsigned int initialValue = 65536;
-  Crypt(data, dataLength, initialValue);
+  //unsigned int initialValue = 0x12345678;
+  std::cout << Crypt(data, dataLength, initialValue) << "\0";
+
 
   return 0;
 }
